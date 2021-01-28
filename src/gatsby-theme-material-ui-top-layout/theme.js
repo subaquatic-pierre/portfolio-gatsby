c23e07b0974a0d547f209e0b5c7db75a7df0f279
Pierre <subaquatic.pierre@gmail.com>
2021-01-28 14:18:56 +0400

    Added typescript to the project

--- src/gatsby-theme-material-ui-top-layout/theme.js
+++ src/gatsby-theme-material-ui-top-layout/theme.js
@@ -1,75 +1,76 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
-const primaryColor = "#16a085"
-const secondaryColor = "#9b59b6"
-const teriairyColor = "#38ef7d"
-
+const primaryColor = "#16a085";
+const secondaryColor = "#9b59b6";
+const teriairyColor = "#38ef7d";
 
 const theme = createMuiTheme({
-    palette: {
-        type: 'light',
-        common: {
-            blue: `${primaryColor}`,
-            orange: `${secondaryColor}`
-        },
-        primary: {
-            main: `${primaryColor}`
-        },
-        secondary: {
-            main: `${secondaryColor}`
-        },
-        gradients: {
-            primary: `linear-gradient(to left, ${primaryColor}, ${teriairyColor})`,
-            secondary: 'this gradient'
-        },
-        shadows: {
-            xssmall: '1px 1px 2px rgba(0,0,0, 0.4)',
-            small: '1px 1px 4px rgba(0,0,0, 0.5)',
-            medium: '1px 1px 4px rgba(0,0,0, 0.8), 1.5px 1.5px 5px rgba(0,0,0, 0.3)',
-            large: '1px 1px 4px rgba(0,0,0, 0.8), 1.5px 1.5px 5px rgba(0,0,0, 0.3),2px 2px 6px rgba(0,0,0, 0.2)',
-            xlarge: '1px 1px 4px rgba(0,0,0, 0.8), 1px 1px 4px rgba(0,0,0, 0.8), 1.5px 1.5px 5px rgba(0,0,0, 0.3),2px 2px 6px rgba(0,0,0, 0.2)',
-        }
+  palette: {
+    type: "light",
+    common: {
+      blue: `${primaryColor}`,
+      orange: `${secondaryColor}`,
     },
-    typography: {
-        tab: {
-            fontFamily: 'Lato',
-            fontWeight: 700,
-            fontSize: '1rem',
-            letterSpacing: '0.1rem',
-            textTransform: 'uppercase',
-            color: 'white',
-            opacity: '0.87',
-            '&:hover': {
-                opacity: '1'
-            }
-        },
-        ocean: {
-            fontFamily: 'Patrick Hand',
-            fontSize: '1.1rem',
-            lineHeight: '1.1rem',
-            color: 'white',
-        },
+    primary: {
+      main: `${primaryColor}`,
     },
-    overrides: {
-        MuiButton: {
-            root: {
-                padding: '0.5rem 2rem',
-                '&:hover': {
-                    backgroundColor: 'none'
-                }
-            }
+    secondary: {
+      main: `${secondaryColor}`,
+    },
+    gradients: {
+      primary: `linear-gradient(to left, ${primaryColor}, ${teriairyColor})`,
+      secondary: "this gradient",
+    },
+    shadows: {
+      xssmall: "1px 1px 2px rgba(0,0,0, 0.4)",
+      small: "1px 1px 4px rgba(0,0,0, 0.5)",
+      medium: "1px 1px 4px rgba(0,0,0, 0.8), 1.5px 1.5px 5px rgba(0,0,0, 0.3)",
+      large:
+        "1px 1px 4px rgba(0,0,0, 0.8), 1.5px 1.5px 5px rgba(0,0,0, 0.3),2px 2px 6px rgba(0,0,0, 0.2)",
+      xlarge:
+        "1px 1px 4px rgba(0,0,0, 0.8), 1px 1px 4px rgba(0,0,0, 0.8), 1.5px 1.5px 5px rgba(0,0,0, 0.3),2px 2px 6px rgba(0,0,0, 0.2)",
+    },
+  },
+  typography: {
+    tab: {
+      fontFamily: "Lato",
+      fontWeight: 700,
+      fontSize: "1rem",
+      letterSpacing: "0.1rem",
+      textTransform: "uppercase",
+      color: "white",
+      opacity: "0.87",
+      "&:hover": {
+        opacity: "1",
+      },
+    },
+    ocean: {
+      fontFamily: "Patrick Hand",
+      fontSize: "1.1rem",
+      lineHeight: "1.1rem",
+      color: "white",
+    },
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        padding: "0.5rem 2rem",
+        "&:hover": {
+          backgroundColor: "none",
         },
-        MuiLink: {
-            root: {
-                color: 'inherit',
-            },
-            underlineHover: {
-                '&:hover': {
-                    textDecoration: 'none',
-                }
-            }
-        }
-    }
-})
+      },
+    },
+    MuiLink: {
+      root: {
+        color: "inherit",
+      },
+      underlineHover: {
+        "&:hover": {
+          textDecoration: "none",
+        },
+      },
+    },
+  },
+});
 
 export default theme;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,214 +1,232 @@
-import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import {
-    Typography,
-    CircularProgress,
-    Grid,
-    Container,
-    FormGroup,
-    FormControlLabel,
-    useScrollTrigger
-} from '@material-ui/core';
-import path from 'path'
+  Typography,
+  CircularProgress,
+  Grid,
+  Container,
+  FormGroup,
+  FormControlLabel,
+  useScrollTrigger,
+} from "@material-ui/core";
+import path from "path";
 
-import Layout from '../components/Layout';
-import ProjectsSwitch from '../components/ProjectsSwitch'
-import Divider from '../components/Divider'
-import Project from '../components/Project'
-import ScrollToTop from '../components/ScrollToTop'
+import Layout from "../components/Layout";
+import ProjectsSwitch from "../components/ProjectsSwitch";
+import Divider from "../components/Divider";
+import Project from "../components/Project";
+import ScrollToTop from "../components/ScrollToTop";
 
-import caps from '../utils/capitalize'
+import caps from "../utils/capitalize";
 
-import productionProjectsData from '../pagedata/productionProjectsData'
-import sideProjectsData from '../pagedata/sideProjectsData'
+import productionProjectsData from "../pagedata/productionProjectsData";
+import sideProjectsData from "../pagedata/sideProjectsData";
 
 const useStyles = makeStyles((theme) => ({
-    layout: {
-        minHeight: '100vh'
+  layout: {
+    minHeight: "100vh",
+  },
+  pageContainer: {
+    paddingTop: "1.5rem",
+    paddingBottom: "2rem",
+  },
+  projectContainer: {
+    paddingBottom: "2rem",
+  },
+  cardCol: {
+    [theme.breakpoints.up("md")]: {
+      padding: "2rem 0",
     },
-    pageContainer: {
-        paddingTop: '1.5rem',
-        paddingBottom: '2rem',
+    padding: "1.2rem 0",
+  },
+  cardRoot: {
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%", // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: "rotate(180deg)",
+  },
+  toggleSection: {
+    [theme.breakpoints.up("sm")]: {
+      margin: "2rem auto",
     },
-    projectContainer: {
-        paddingBottom: '2rem',
+    padding: "1rem",
+    border: `1px solid ${theme.palette.grey[300]}`,
+    borderRadius: "5px",
+    margin: "1rem auto",
+    "& span": {
+      fontWeight: "bold",
     },
-    cardCol: {
-        [theme.breakpoints.up('md')]: {
-            padding: '2rem 0'
-        },
-        padding: '1.2rem 0'
-    },
-    cardRoot: {
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-    },
-    media: {
-        height: 0,
-        paddingTop: '56.25%', // 16:9
-    },
-    expand: {
-        transform: 'rotate(0deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    toggleSection: {
-        [theme.breakpoints.up('sm')]: {
-            margin: '2rem auto'
-        },
-        padding: '1rem',
-        border: `1px solid ${theme.palette.grey[300]}`,
-        borderRadius: '5px',
-        margin: '1rem auto',
-        '& span': {
-            fontWeight: 'bold',
-        },
-    },
-    bottomContent: {
-        marginTop: 'auto'
-    },
-    techList: {
-        paddingTop: '0',
-        paddingBottom: '0'
-    }
+  },
+  bottomContent: {
+    marginTop: "auto",
+  },
+  techList: {
+    paddingTop: "0",
+    paddingBottom: "0",
+  },
 }));
 
 const Projects = () => {
-    const classes = useStyles();
-    const [expandId, setExpandId] = useState(-1)
-    const [loading, setLoading] = useState(false)
-    const [numProdProjects, setNumProdProjects] = useState(6)
-    const [numSideProjects, setNumSideProjects] = useState(3)
-    const [sideProjectPage, setSideProjectPage] = useState(false)
-    const [projectData, setProjectData] = useState([])
+  const classes = useStyles();
+  const [expandId, setExpandId] = useState(-1);
+  const [loading, setLoading] = useState(false);
+  const [numProdProjects, setNumProdProjects] = useState(6);
+  const [numSideProjects, setNumSideProjects] = useState(3);
+  const [sideProjectPage, setSideProjectPage] = useState(false);
+  const [projectData, setProjectData] = useState([]);
 
-    const handleExpandClick = (index) => {
-        setExpandId(expandId === index ? -1 : index)
-    };
+  const handleExpandClick = (index) => {
+    setExpandId(expandId === index ? -1 : index);
+  };
 
-    const handleProductionProjectSwitch = async (event) => {
-        removeInfiniteScrollListener()
-        setExpandId(-1)
-        setSideProjectPage(sideProjectPage => !sideProjectPage)
-    };
+  const handleProductionProjectSwitch = async (event) => {
+    removeInfiniteScrollListener();
+    setExpandId(-1);
+    setSideProjectPage((sideProjectPage) => !sideProjectPage);
+  };
 
-    // shrink app bar on scroll
-    const shrinkTrigger = useScrollTrigger({
-        disableHysteresis: true,
-        threshold: 100,
-    });
+  // shrink app bar on scroll
+  const shrinkTrigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
 
-    // Add or remove event listener
-    const addInfiniteScrollListener = () => {
-        return window.addEventListener('scroll', handleInfiniteScroll);
+  // Add or remove event listener
+  const addInfiniteScrollListener = () => {
+    return window.addEventListener("scroll", handleInfiniteScroll);
+  };
+
+  const removeInfiniteScrollListener = () => {
+    return window.removeEventListener("scroll", handleInfiniteScroll);
+  };
+
+  const updateProjects = () => {
+    addInfiniteScrollListener();
+    if (sideProjectPage) {
+      setProjectData(sideProjectsData.slice(0, numSideProjects));
+    } else {
+      setProjectData(productionProjectsData.slice(0, numProdProjects));
     }
+    setLoading(false);
+  };
 
-    const removeInfiniteScrollListener = () => {
-        return window.removeEventListener('scroll', handleInfiniteScroll);
-    }
+  const scrollUpdate = () => {
+    setTimeout(updateProjects, 2000);
+  };
 
-    const updateProjects = () => {
-        addInfiniteScrollListener()
-        if (sideProjectPage) {
-            setProjectData(sideProjectsData.slice(0, numSideProjects))
+  const handleInfiniteScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >=
+      document.body.offsetHeight + 100
+    ) {
+      removeInfiniteScrollListener();
+      if (sideProjectPage) {
+        // Check within data index
+        if (numSideProjects < sideProjectsData.length) {
+          setLoading(true);
+          setNumSideProjects(numSideProjects + 3);
         } else {
-            setProjectData(productionProjectsData.slice(0, numProdProjects))
+          setLoading(false);
         }
-        setLoading(false)
-    }
-
-    const scrollUpdate = () => {
-        setTimeout(updateProjects, 2000)
-    }
-
-    const handleInfiniteScroll = () => {
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight + 100) {
-            removeInfiniteScrollListener()
-            if (sideProjectPage) {
-                // Check within data index
-                if (numSideProjects < sideProjectsData.length) {
-                    setLoading(true)
-                    setNumSideProjects(numSideProjects + 3)
-                } else {
-                    setLoading(false)
-                }
-            } else {
-                // Check within data index
-                if (numProdProjects < productionProjectsData.length) {
-                    setLoading(true)
-                    setNumProdProjects(numProdProjects + 3)
-                } else {
-                    setLoading(false)
-                }
-            }
+      } else {
+        // Check within data index
+        if (numProdProjects < productionProjectsData.length) {
+          setLoading(true);
+          setNumProdProjects(numProdProjects + 3);
+        } else {
+          setLoading(false);
         }
+      }
     }
+  };
 
-    useEffect(() => {
-        updateProjects()
-    }, [sideProjectPage])
+  useEffect(() => {
+    updateProjects();
+  }, [sideProjectPage]);
 
-    useEffect(() => {
-        scrollUpdate()
-    }, [numSideProjects, numProdProjects])
+  useEffect(() => {
+    scrollUpdate();
+  }, [numSideProjects, numProdProjects]);
 
-    useEffect(() => {
-        addInfiniteScrollListener()
-        setProjectData(productionProjectsData.slice(0, numProdProjects))
-    }, [])
+  useEffect(() => {
+    addInfiniteScrollListener();
+    setProjectData(productionProjectsData.slice(0, numProdProjects));
+  }, []);
 
-    const title = path.basename(__filename).split('.')[0]
+  const title = path.basename(__filename).split(".")[0];
 
-    return (
-        <Layout title={caps(title)}>
-            <Container className={classes.layout} maxWidth='lg' >
-                <Grid container item xs={12} className={classes.pageContainer}>
-                    <Grid container className={classes.headingSection} direction='column' justify='center' alignItems='center'>
-                        <Typography variant='h2' component='h1' align='center'>
-                            Projects
-                    </Typography>
-                        <Divider width={20} space={1.5} color='secondary' />
-                    </Grid>
-                    <Grid container className={classes.toggleSection} item xs={12} sm={8}>
-                        <Typography variant='body1'>
-                            Toggle between production projects and non-production projects. Non-production projects may not be live, all source code can be found in the GitHub repo.
-                    </Typography>
-                        <FormGroup>
-                            <br />
-                            <FormControlLabel
-                                control={<ProjectsSwitch checked={sideProjectPage} onChange={handleProductionProjectSwitch} />}
-                                label="Non-Production Projects"
-                            />
-                        </FormGroup>
-                    </Grid>
-                </Grid>
-                <Grid container spacing={3} className={classes.projectContainer} item>
-                    {projectData && projectData.map((project, idx) => (
-                        <Project
-                            key={idx}
-                            index={idx}
-                            item={project}
-                            expandId={expandId}
-                            handleExpandClick={handleExpandClick}
-                        />
-                    ))}
-                    {loading &&
-                        <Grid container justify='center'>
-                            <CircularProgress disableShrink />
-                        </Grid>
-                    }
-                </Grid>
-            </Container >
-            <ScrollToTop trigger={shrinkTrigger} color='secondary' size='small' />
-        </Layout>
-    );
-}
+  return (
+    <Layout title={caps(title)}>
+      <Container className={classes.layout} maxWidth="lg">
+        <Grid container item xs={12} className={classes.pageContainer}>
+          <Grid
+            container
+            className={classes.headingSection}
+            direction="column"
+            justify="center"
+            alignItems="center"
+          >
+            <Typography variant="h2" component="h1" align="center">
+              Projects
+            </Typography>
+            <Divider width={20} space={1.5} color="secondary" />
+          </Grid>
+          <Grid container className={classes.toggleSection} item xs={12} sm={8}>
+            <Typography variant="body1">
+              Toggle between production projects and Github projects, not all
+              Github projects are live. The source code for each project can be
+              found on the Github button on each project card, some source codes
+              have been omitted for security reasons.
+            </Typography>
+            <FormGroup>
+              <br />
+              <FormControlLabel
+                control={
+                  <ProjectsSwitch
+                    checked={sideProjectPage}
+                    onChange={handleProductionProjectSwitch}
+                  />
+                }
+                label="Github Projects"
+              />
+            </FormGroup>
+          </Grid>
+        </Grid>
+        <Grid container spacing={3} className={classes.projectContainer} item>
+          {projectData &&
+            projectData.map((project, idx) => (
+              <Project
+                key={idx}
+                index={idx}
+                item={project}
+                expandId={expandId}
+                handleExpandClick={handleExpandClick}
+              />
+            ))}
+          {loading && (
+            <Grid container justify="center">
+              <CircularProgress disableShrink />
+            </Grid>
+          )}
+        </Grid>
+      </Container>
+      <ScrollToTop trigger={shrinkTrigger} color="secondary" size="small" />
+    </Layout>
+  );
+};
 
 export default Projects;
